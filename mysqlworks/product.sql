434c317906f2ca8e30372575b3c63a0a93d9567e
codingslave2 <codingslavekim121@gmail.com>
2023-07-20 18:25:16 +0900

    20230720

--- mysqlworks/product.sql
+++ mysqlworks/product.sql
@@ -2,15 +2,22 @@ use javaweb;
 
 -- product table
 
-create table product(
-	p_id varchar(10) primary key,
-    p_name varchar(30),
-    p_unitPrice integer,
-    p_description text,
-    p_category varchar(20),
-    p_manufacturer varchar(20),
-    p_unitsInStork long,
-    p_condition varchar(20),
-    p_productImage varchar(20)
-
+CREATE TABLE product(
+	p_id	VARCHAR(10) PRIMARY KEY,
+    p_name  VARCHAR(30),
+    p_unitPrice INTEGER,
+    p_description TEXT,
+    p_category  VARCHAR(20),
+    p_manufacturer VARCHAR(20),
+    p_unitsInStock LONG,
+    p_condition VARCHAR(20),
+    p_productImage VARCHAR(20)
 );
+
+-- 상품 추가
+INSERT INTO product VALUES ('P1234', 'iPhone 6s', 1200000, '1334X750 Renia HD Display',
+			'Smart Phone', 'Apple', 1000, 'New', 'P1234.png');
+
+select * from product;
+
+drop table product;
